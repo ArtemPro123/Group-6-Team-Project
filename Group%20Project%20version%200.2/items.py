9cbe69534ce464bedc2d1df307f137d68fddb66a
LukeDJ24 <lukedjones23@gmail.com>
2016-10-16 18:57:39 +0100

    Add files via upload
    Items list is just a few items to kick off, we can add more as we go. Again map is just our basic map layout.

---
+++ Group%20Project%20version%200.2/items.py
@@ -0,0 +1,74 @@
+item_basic_armor = {
+    "id": "basic armor",
+
+    "name": "basic armor",
+
+    "description":
+    """Some basic armor you made out of wood and string. It doesn't look very sturdy!"""
+}
+
+item_map = {
+    "id": "map",
+
+    "name": "tatty old map",
+
+    "description":
+    "A tatty old map you found at the back of a cupboard"
+}
+
+item_coins = {
+    "id": "coins",
+
+    "name": "coins",
+
+    "description":
+    "Your life savings gingle in a leather sack."
+}
+
+item_food = {
+    "id": "food",
+
+    "name": "bag of food",
+
+    "description": "Tastes so good you can feel yourself getting better!"
+}
+
+item_axe = {
+    "id": "axe",
+    
+    "name": "an axe",
+
+    "description": "A sturdy axe. Used by local woodcutters. The axe edge shines brightly!"
+}
+
+item_shield = {
+    "id": "shield",
+    
+    "name": "the shield of valshara",
+
+    "description": "This shield has seen many battles. Rumoured to be the shield of the greatest warrior in the land! How'd it end up in this shop?!"
+}
+
+item_wood = {
+    "id": "wood",
+    
+    "name": "wood",
+
+    "description": "Cut wood from deep in the forest. This strong wood is perfect for building."
+}
+
+item_beer = {
+    "id": "beer",
+    
+    "name": "beer",
+
+    "description": "The beer is a dark brown with bits in. Don't worry how it made, just drink it."
+}
+
+item_2handedsword = {
+	"id": "2handedsword",
+    
+    "name": "2 handed sword of the giant killer",
+
+    "description": "This massive 2 handed sword belonged to a soldier at the abandoned camp."
+}
