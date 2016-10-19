43ca409c7038007b732655799b029ea9d94f2bc2
LukeDJ24 <lukedjones23@gmail.com>
2016-10-19 13:45:51 +0100

    Update player.py

--- Game 0.9/player.py
+++ Game 0.9/player.py
@@ -5,25 +5,25 @@ warrior = {
     "inventory": [item_sword],
     #"gold": int(50),
     "hp": int(200),
-    "temp_hp": int(150)
+    "temp_hp": int(200)
     }
 mage = {
     "inventory": [item_mage_staff],
     #"gold" = int(50),
-    "hp": int(100),
+    "hp": int(150),
     "temp_hp": int(150)
     }
 rogue = { 
     "inventory": [item_dagger, item_helping_hand],
     #"gold" = int(50),
     "hp": int(100),
-    "temp_hp": int(70)
+    "temp_hp": int(100)
     } 
 cleric = { 
     "inventory":[item_mace, item_restoration_potion],
     #"gold" = int(50),
     "hp": int(130),
-    "temp_hp": int(80)
+    "temp_hp": int(130)
     } 
 
 current_room = rooms["Home"]
