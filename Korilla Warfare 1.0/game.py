eb397e090fabc9d489110b7e66eaa97703b7f10b
Nageki4Ever <deeonroy@gmail.com>
2016-10-21 12:33:49 +0100

    Update game.py

--- Korilla Warfare 1.0/game.py
+++ Korilla Warfare 1.0/game.py
@@ -505,7 +505,9 @@ def combat():
 								if item["type"] in type_attack: 
 									if item in inventory:  
 										gold = (item["cost"]*0.5) + gold
-										print("you gained " + str((item["cost"]*0.5)) + " Gold")  
+										print("you gained " + str((item["cost"]*0.5)) + " Gold")
+									else: 
+										inventory.append(item)
 								else: 
 									inventory.append(item)
 								print("The enemy dropped a " + item["name"])
